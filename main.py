29f5cc243f059b66c414511c734114631200774d
dontebailey <baileydonte27@gmail.com>
2024-01-31 10:17:12 -0500

    was able to append data looping through test_data

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from serpapi import GoogleSearch
 import json
 
+page_counter = 0
+
 params = {
     "engine": "google_jobs",
     "q": "software developer",
@@ -10,14 +12,24 @@ params = {
     "hl": "en",
     "gl": "us",
     "location": "Boston, Massachusetts, United States",
-    "start": "0",
+    "start": page_counter,
     "api_key": secrets.secret_key
 }
+
+output_file = Path("output_file.json")
 data_file = Path("test_data.json")
 
 # search = GoogleSearch(params)
 # results = search.get_dict()
-# data_file.write_text(json.dumps(results))
 
-data = json.loads(data_file.read_text())
-print(data)
+data_results = []
+
+# data_file.write_text(json.dumps(results.get("jobs_results")))
+# while search.params_dict("start") <= 40:
+while page_counter <= 40:
+    data_results.append(json.loads(data_file.read_text()))
+    page_counter += 10
+
+output_file.write_text(json.dumps(data_results))
+
+
