bb84fc9f2712a70444d1453e99106a1a63e75a0e
dontebailey <baileydonte27@gmail.com>
2024-01-31 11:30:49 -0500

    Able to retrieve 5 pages of data and store into json file

--- main.py
+++ main.py
@@ -16,20 +16,17 @@ params = {
     "api_key": secrets.secret_key
 }
 
-output_file = Path("output_file.json")
-data_file = Path("test_data.json")
+data_file = Path("data_file.json")
 
-# search = GoogleSearch(params)
-# results = search.get_dict()
+search = GoogleSearch(params)
+results = search.get_dict()
 
 data_results = []
 
-# data_file.write_text(json.dumps(results.get("jobs_results")))
-# while search.params_dict("start") <= 40:
 while page_counter <= 40:
-    data_results.append(json.loads(data_file.read_text()))
+    data_results.append(results.get("jobs_results"))
     page_counter += 10
 
-output_file.write_text(json.dumps(data_results))
+data_file.write_text(json.dumps(data_results))
 
 
