944ceb7c05fec5876271fb0114b853777bbc3861
dontebailey <baileydonte27@gmail.com>
2024-01-31 21:22:06 -0500

    created functions

--- main.py
+++ main.py
@@ -3,30 +3,40 @@ from pathlib import Path
 from serpapi import GoogleSearch
 import json
 
-page_counter = 0
 
-params = {
-    "engine": "google_jobs",
-    "q": "software developer",
-    "google_domain": "google.com",
-    "hl": "en",
-    "gl": "us",
-    "location": "Boston, Massachusetts, United States",
-    "start": page_counter,
-    "api_key": secrets.secret_key
-}
+def get_data():
+    page_counter = 0
 
-data_file = Path("data_file.json")
+    params = {
+        "engine": "google_jobs",
+        "q": "software developer",
+        "google_domain": "google.com",
+        "hl": "en",
+        "gl": "us",
+        "location": "Boston, Massachusetts, United States",
+        "start": page_counter,
+        "api_key": secrets.secret_key
+    }
 
-search = GoogleSearch(params)
-results = search.get_dict()
+    search = GoogleSearch(params)
+    results = search.get_dict()
 
-data_results = []
+    data_results = []
 
-while page_counter <= 40:
-    data_results.append(results.get("jobs_results"))
-    page_counter += 10
+    while page_counter <= 40:
+        data_results.append(results.get("jobs_results"))
+        page_counter += 10
 
-data_file.write_text(json.dumps(data_results))
+    return data_results
 
 
+def save_data():
+    data_file = Path("data_file.json")
+    data_file.write_text(json.dumps(get_data()))
+
+
+def main():
+    save_data()
+
+
+main()
