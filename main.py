f261e6c469baf24093f3172c453cd5cab47f3646
dontebailey <baileydonte27@gmail.com>
2024-02-06 21:20:26 -0500

    able to get specific job data and print to terminal

--- main.py
+++ main.py
@@ -7,9 +7,33 @@ from typing import Tuple
 
 
 def get_data():
+    # page_counter = 0
+    #
+    # params = {  # add comment to test workflow
+    #     "engine": "google_jobs",
+    #     "q": "software developer",
+    #     "google_domain": "google.com",
+    #     "hl": "en",
+    #     "gl": "us",
+    #     "location": "Boston, Massachusetts, United States",
+    #     "start": page_counter,
+    #     "api_key": secrets.secret_key
+    # }
+    #
+    # search = GoogleSearch(params)
+    # results = search.get_dict()
+    #
+    # data_results = []
+    #
+    # while page_counter <= 40:
+    #     data_results.append(results.get("jobs_results"))
+    #     page_counter += 10
+    #
+    # return data_results
+
     page_counter = 0
 
-    params = {  # add comment to test workflow
+    params = {
         "engine": "google_jobs",
         "q": "software developer",
         "google_domain": "google.com",
@@ -20,16 +44,33 @@ def get_data():
         "api_key": secrets.secret_key
     }
 
-    search = GoogleSearch(params)
-    results = search.get_dict()
+    data_file = Path("data_file.json")
 
-    data_results = []
+    # search = GoogleSearch(params)
+    # results = search.get_dict()
+    #
+    # data_results = []
+    #
+    # while page_counter <= 40:
+    #     data_results.append(results.get("jobs_results"))
+    #     page_counter += 10
 
-    while page_counter <= 40:
-        data_results.append(results.get("jobs_results"))
-        page_counter += 10
+    # data_file.write_text(json.dumps(data_results))
 
-    return data_results
+    # opening json file
+    # file = open("data_file.json")
+
+    # returns JSON object as a dictionary
+    # data = json.loads(Path("data_file.json").read_text())
+
+    data = json.loads(data_file.read_text())
+
+    for page in data:
+        for job in page:
+            company_name = job["company_name"]
+            location = job["location"]
+            print(company_name)
+            print(location)
 
 
 def save_data():
@@ -70,10 +111,12 @@ def save_to_db():
 
 
 def main():
-    conn, cursor = open_db("google_jobs_db.sqlite")
-    setup_db(cursor)
-    generate_jobs(cursor)
-    close_db(conn)
+    # conn, cursor = open_db("google_jobs_db.sqlite")
+    # setup_db(cursor)
+    # generate_jobs(cursor)
+    # close_db(conn)
+    # save_data()
+    get_data()
 
 
 main()
