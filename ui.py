b2ede5c35f27806d463e1592e95f0e73cb44e9be
dontebailey <baileydonte27@gmail.com>
2024-02-26 17:14:52 -0500

    able to fix db connection and put job_listings into AgGrid

--- ui.py
+++ ui.py
@@ -3,23 +3,22 @@ from dash import Dash, html
 import dash_ag_grid as dag
 import pandas as pd
 
-
 # create a connection to database
-conn = sqlite3.connect("")
+conn = sqlite3.connect("Comp490Jobs.sqlite")
 data = pd.read_sql_query("Select * from jobs_listings", conn)
 conn.close()
 app = Dash(__name__)
 
 columnDefs = [
-    {"field": "job id"},
-    {"field": "job title"},
-    {"field": "company name"},
-    {"field": "job description"},
+    {"field": "job_id"},
+    {"field": "job_title"},
+    {"field": "company_name"},
+    {"field": "job_description"},
     {"field": "location"},
-    {"field": "min-salary"},
-    {"field": "max-salary"},
-    {"field": "salary time"},
-    {"field": "posted at"},
+    {"field": "min_salary"},
+    {"field": "max_salary"},
+    {"field": "salary_time"},
+    {"field": "posted_at"},
     {"field": "url"},
     {"field": "remote"},
 ]
