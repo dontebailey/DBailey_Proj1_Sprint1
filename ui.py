d29787d122a9707ca398441791caae667a384fdc
dontebailey <baileydonte27@gmail.com>
2024-02-27 13:34:18 -0500

    fixed columns to headline info and resized them to fit screen

--- ui.py
+++ ui.py
@@ -10,29 +10,29 @@ conn.close()
 app = Dash(__name__)
 
 columnDefs = [
-    {"field": "job_id", "headerName": "Job Id"},
-    {"field": "job_title", "headerName": "Job Title"},
-    {"field": "company_name", "headerName": "Company Name"},
-    {"field": "job_description", "headerName": "Job Description"},
-    {"field": "location", "headerName": "Location"},
-    {"field": "min_salary", "headerName": "Minimum salary"},
-    {"field": "max_salary", "headerName": "Maximum salary"},
-    {"field": "salary_time", "headerName": "Salary Time"},
-    {"field": "posted_at", "headerName": "Posted"},
-    {"field": "url", "headerName": "URL"},
-    {"field": "remote", "headerName": "Remote"},
+    # {"field": "job_id", "headerName": "Job ID"},
+    {"field": "job_title", "headerName": "Job Title", "sizeToFit": True},
+    {"field": "company_name", "headerName": "Company Name", "sizeToFit": True},
+    # {"field": "job_description", "headerName": "Job Description"},
+    {"field": "location", "headerName": "Location", "sizeToFit": True},
+    # {"field": "min_salary", "headerName": "Minimum salary"},
+    # {"field": "max_salary", "headerName": "Maximum salary"},
+    # {"field": "salary_time", "headerName": "Salary Time"},
+    # {"field": "posted_at", "headerName": "Posted"},
+    # {"field": "url", "headerName": "URL"},
+    # {"field": "remote", "headerName": "Remote"},
 ]
 
 grid = dag.AgGrid(
     id="jobs-grid",
     rowData=data.to_dict("records"),
-    columnDefs=columnDefs
+    columnDefs=columnDefs,
+    columnSize="responsiveSizeToFit"
 )
 
-app.layout = html.Div([
-    # dcc.Textarea(readOnly=True, id="jobs_display_window", style={"width": "30%", "height": 400})
-    grid
-])
+app.layout = html.Div([grid])
+# dcc.Textarea(readOnly=True, id="jobs_display_window", style={"width": "30%", "height": 400})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
